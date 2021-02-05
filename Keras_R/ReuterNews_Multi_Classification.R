ed4302af7e63f60416edf7053f7d7c5d2bbea7f9
Seonghun Jeong <ikeven@naver.com>
2021-02-05 14:14:16 +0900

    keras modeling with reuter news

--- Keras_R/ReuterNews_Multi_Classification.R
+++ Keras_R/ReuterNews_Multi_Classification.R
@@ -61,6 +61,7 @@ model %>% compile(
   metrics = c('accuracy')
 )
 
+# Validation Set
 val_indices <- 1:1000
 x_val <- x_train[val_indices,]
 partial_x_train <- x_train[-val_indices,]
@@ -75,6 +76,7 @@ history <- model %>%  fit(
   validation_data = list(x_val,y_val)
 )
 plot(history)
+# 9 epoch 이후 overfitting
 
 results <- model %>% evaluate(x_test,one_hot_test_labels)
 results
@@ -101,3 +103,89 @@ history <- model %>% fit(
 
 results <- model %>% evaluate(x_test,one_hot_test_labels)
 results
+# > results
+# loss  accuracy 
+# 0.9728372 0.7902939
+
+# 정확도 79%
+
+test_labels_copy <- test_labels
+test_labels_copy <- sample(test_labels_copy)
+length(which(test_labels == test_labels_copy))/length(test_labels)
+# [1] 0.1821015 (이항 분류 시 정확도)
+
+
+predictions <- model %>% predict(x_test)
+dim(predictions)
+which.max(predictions[1,])
+
+
+
+
+
+
+
+
+
+
+
+# validation accuracy ( 은닉 계층 1개일 경우)
+4 > 0.70 (0.05 감소)
+32 > 0.79
+64 > 0.80
+128 >  0.80
+
+
+model <- keras_model_sequential() %>% 
+  layer_dense(units = 64, activation = 'relu', input_shape = c(10000)) %>% 
+  layer_dense(units = 128, activation = 'relu') %>% 
+  layer_dense(units = 46, activation = 'softmax')
+
+model %>%  compile(
+  optimizer = 'adam',
+  loss = 'categorical_crossentropy',
+  metrics = c('accuracy')
+)
+
+history <- model %>% fit(
+  partial_x_train,
+  partial_y_train,
+  epochs = 20,
+  batch_size = 512,
+  validation_data = list(x_val,y_val)
+)
+
+
+
+# validation accuracy ( 은닉 계층 2개일 경우)
+4*4 > 0.64
+16*16 > 0.64
+32*32  > 0.78
+64*64 > 0.80
+128*128 > 0.80
+128*64 > 0.80
+# >> 일정 수준 이후에는 깊어져도 증가하지 않는다.
+
+model <- keras_model_sequential() %>% 
+  layer_dense(units = 64, activation = 'relu', input_shape = c(10000)) %>% 
+  layer_dense(units = 16, activation = 'relu') %>%
+  layer_dense(units = 32, activation = 'relu') %>% 
+  layer_dense(units = 46, activation = 'softmax')
+
+model %>%  compile(
+  optimizer = 'adam',
+  loss = 'categorical_crossentropy',
+  metrics = c('accuracy')
+)
+
+history <- model %>% fit(
+  partial_x_train,
+  partial_y_train,
+  epochs = 20,
+  batch_size = 512,
+  validation_data = list(x_val,y_val)
+)
+
+
+
+
